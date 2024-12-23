46fd2b570cd1073648be3521fb807f5e81adb94f
Soumya Chakraborty <soumya.chakraborty@iiml.org>
2024-12-23 10:57:02 +0530

    Set up the front end styling classes and organize them future scalability

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,23 @@
 function App() {
   return (
     <>
-      <div className="w-full bg-slate-700 text-white">Start</div>
+      <div className="body-container">
+        <div className="navbar">
+          <div className="navbar-content">Start</div>
+        </div>
+        <div className="content">
+          <div className="checkout-body-container">
+            <div className="checkout-body">
+              <div>body start</div>
+            </div>
+          </div>
+        </div>
+        <footer className="footer">
+          <div className="footer-box">
+            <div>Footer</div>
+          </div>
+        </footer>
+      </div>
     </>
   );
 }
