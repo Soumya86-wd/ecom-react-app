30a0e76302592f01b4d3cd7ce129d0a333deb6db
Soumya Chakraborty <soumya.chakraborty@iiml.org>
2024-12-25 22:14:02 +0530

    Create index file for the services

--- server-postgres/src/services/index.ts
+++ server-postgres/src/services/index.ts
@@ -1,1 +1,19 @@
-// exports from the services folder
+import { BrandService } from "./brand.service";
+import { CartService } from "./cart.service";
+import { CategoryService } from "./category.service";
+import { DeliveryOptionService } from "./deliveryOption.service";
+import { OrderService } from "./order.service";
+import { ProductService } from "./product.service";
+import { TransactionItemService } from "./transactionItem.service";
+import { UserService } from "./user.service";
+
+export {
+  BrandService,
+  CartService,
+  CategoryService,
+  DeliveryOptionService,
+  OrderService,
+  ProductService,
+  TransactionItemService,
+  UserService,
+};
